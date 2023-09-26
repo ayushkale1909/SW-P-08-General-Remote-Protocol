2a1a1918ef8db7d90b2953d7da92193ef1f874d7
ayush kale <74785780+ayushkale1909@users.noreply.github.com>
2023-09-26 13:39:02 -0400

    Add files via upload

---
+++ crosspoint_interrogate_message_example.py
@@ -0,0 +1,61 @@
+import socket
+som = bytes([0x10, 0x02])
+command = bytes([0x03])
+eom = bytes([0x10, 0x03])
+btc = 4
+
+def calculate_byte_1(matrix, level):
+    if not (0 < matrix <= 16) or not (0 < level <= 16):
+        raise ValueError("Matrix and Level numbers between 1 and 16")
+    value = (matrix - 1) << 4 | (level - 1)
+    return value
+
+def calculate_byte_2(source_number, dest_number):
+    source_value = (source_number // 128) & 0b111
+    dest_value = (dest_number // 128) & 0b111
+    byte_value = (dest_value << 4) | source_value
+    byte_value &= 0b01110111  
+    return byte_value
+
+def calculate_byte_3(dest_number):
+    return dest_number % 128
+
+def compute_checksum(data):
+    total = sum(data) % 256
+    return (256 - total) % 256
+
+def send_message(message, ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(60)
+        sock.connect((ip, port))
+        print(f"Connected to {ip}:{port}")
+        sock.send(message)
+        print(f"Message sent.")
+        reply = sock.recv(1024)
+        print(f"Received reply: {reply}")
+
+    except socket.timeout:
+        print(f"Timeout: No reply received from {ip}:{port} within the set timeout period.")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        sock.close()
+
+
+matrix = 2
+level = 1
+source = 5  
+dest = 3
+byte_1_value = calculate_byte_1(matrix, level)
+byte_1 = bytes([byte_1_value])
+byte_2 = calculate_byte_2(source, dest)
+byte_3 = calculate_byte_3(dest)
+chk_data = command + byte_1 + bytes([byte_2, byte_3, btc])
+chk = compute_checksum(chk_data)
+message = som + command + byte_1 + bytes([byte_2, byte_3, btc, chk])+eom
+print(message)
+ip_address = "192.168.10.28"
+port_number = 62010
+send_message(message, ip_address, port_number)
+
---
+++ crosspoint_tally_dump_request_message_all_values.py
@@ -0,0 +1,54 @@
+import socket
+som = bytes([0x10, 0x02])
+command = bytes([0x15])
+# can be 0x16, 0x17 
+eom = bytes([0x10, 0x03])
+btc = 2
+responses = {}
+def calculate_byte_1(matrix, level):
+    if not (0 < matrix <= 16) or not (0 < level <= 16):
+        raise ValueError("Matrix and Level numbers between 1 and 16")
+    value = (matrix - 1) << 4 | (level - 1)
+    return value
+
+def compute_checksum(data):
+    total = sum(data) % 256
+    return (256 - total) % 256
+
+def send_message(message, ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(60)
+        sock.connect((ip, port))
+        print(f"Connected to {ip}:{port}")
+        sock.send(message)
+        print(f"Message sent.")
+        reply = sock.recv(1024)
+        print(f"Received reply: {reply}")
+
+    except socket.timeout:
+        print(f"Timeout: No reply received from {ip}:{port} within the set timeout period.")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        sock.close()
+
+ip_address = "192.168.10.28"
+port_number = 62010
+
+for matrix in range(1, 17):
+    for level in range(1, 17):
+        for source in range(1, 101):
+            for dest in range(1, 101):
+                byte_1_value = calculate_byte_1(matrix, level)
+                byte_1 = bytes([byte_1_value])
+                chk_data = command + byte_1 + bytes([btc])
+                chk = compute_checksum(chk_data)
+                message = som + command + byte_1 + bytes([btc, chk]) + eom
+                key = (matrix, level, source, dest)
+                response = send_message(message, ip_address, port_number)
+                responses[key] = response
+
+
+for key, value in responses.items():
+    print(f"Key: {key}, Response: {value}")
